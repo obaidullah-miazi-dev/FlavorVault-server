e799aee0b00fc69ba74cf198a52b166d7c820828
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-11-23 19:37:09 +0600

    added specific recipe get api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const cors = require('cors')
 const app = express();
@@ -37,6 +37,13 @@ async function run() {
         res.send(result)
     })
 
+
+    app.get('/recipes/:id',async(req,res)=>{
+        const id = req.params.id 
+        const query = {_id: new ObjectId(id)}
+        const result = await recipesCollection.findOne(query)
+        res.send(result)
+    })
 
 
 
