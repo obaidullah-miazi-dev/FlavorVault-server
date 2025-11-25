ce94e7d7a5ed84da5d1ebca42dfeb247719b28fd
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-11-25 22:56:43 +0600

    added delete api

--- index.js
+++ index.js
@@ -52,7 +52,12 @@ async function run() {
     })
 
 
-
+    app.delete('/deleteRecipe/:id',async(req,res)=>{
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await recipesCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
 
